6f4e0a5c68847f4ecacbe3f4b5d46a35f89c0fa0
Willinton Mora <wifermen@hotmail.com>
2022-06-10 19:31:30 -0500

    add copyright on footer

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
 import { GifGrid } from "./components/GifGrid";
 
-const GifExpertApp = ({defaultCategories = []}) => {
+const GifExpertApp = ({ defaultCategories = [] }) => {
 
     // const [category, setCategory] = useState(['Naruto']); 
     const [category, setCategory] = useState(defaultCategories);
@@ -10,18 +10,21 @@ const GifExpertApp = ({defaultCategories = []}) => {
     return (
         <>
             <h1 className="animate__animated animate__fadeInRightBig">GifExpertApp</h1>
-            <AddCategory setCategory={setCategory}/>
+            <AddCategory setCategory={setCategory} />
             <hr />
-            <ol>
-                {
-                    category.map((category) => 
-                        <GifGrid
-                            key={category}
-                            category={category}
-                         />
-                    )
-                }
-            </ol>
+            <div>
+                <ol>
+                    {
+                        category.map((category) =>
+                            <GifGrid
+                                key={category}
+                                category={category}
+                            />
+                        )
+                    }
+                </ol>
+            </div>
+            <footer>Copyright &copy; 2022 - Willinton Mora</footer>
         </>
     )
 }
